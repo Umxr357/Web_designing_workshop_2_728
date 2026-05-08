e304f72cee75a41cf40568ec6d73971ad3b24510
umxr357 <khanumar8369@gmail.com>
2026-05-09 04:07:24 +0530

    updated

--- JavaScript/how to run code/app.js
+++ JavaScript/how to run code/app.js
@@ -1,4 +1,13 @@
-console.log("Hello from java script !");
-function showAlert(){
-    alert("Hello cse-22");
+let fav="Avatar";
+
+let guess=prompt("Guess my fav movie?");
+
+while((guess!=fav) && (guess!="quit")){
+    console.log("Wrong");
+    guess=prompt("Guess again! write quit if you wanna quit");
+}
+if(guess=="Avatar"){
+    console.log("Congrats on guessing it right.");
+}else{
+    console.log("you quit");
 }
